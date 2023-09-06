0fb6a46e051ba86bda2c0e65f934c46fb245a4ff
Wilder <wilderruizrpg@gmail.com>
2023-09-05 19:06:50 -0500

    feat(CrossOrigin): created Cors configuration

--- src/main/java/com/nocountry/ecommerce/config/CrossOriginConfig.java
+++ src/main/java/com/nocountry/ecommerce/config/CrossOriginConfig.java
@@ -1,4 +1,18 @@
 package com.nocountry.ecommerce.config;
 
-public class CrossOriginConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrossOriginConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        
+        registry.addMapping("/**")
+                .allowedOrigins("https://meliclon.web.app") 
+                .allowedMethods("GET","POST","PUT", "DELETE")
+                .allowedHeaders("*");
+    }
 }
